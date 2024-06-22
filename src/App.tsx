4d6e8c179c79f004c37a8b747731b751709d9b38
もっくん <mokkun@M3-MacBookAir.local>
2024-06-23 08:00:30 +0900

    スタイル完成

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import Button from "./components/Button";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [timer, setTimer] = useState(false);
+  const [isBreak, setIsBreak] = useState(false);
 
+  const timerStart = () => {
+    setTimer(true);
+  };
+
+  const timerStop = () => {
+    setTimer(false);
+  };
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+    <div className="">
+      <div className="bg-gray-100 px-5 m-5 shadow-lg rounded-3xl">
+        <p className="text-center pt-5">
+          {isBreak ? "Break Time" : "Work Time"}
         </p>
+        <p className="text-[120px] text-center leading-none pb-5">12:12</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <div className="text-center">
+        <Button className="bg-blue-400" onClick={timerStart}>
+          スタート
+        </Button>
+        <Button className="bg-green-400" onClick={timerStop}>
+          一時停止
+        </Button>
+        <Button className="bg-red-400">終了</Button>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Button.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+type ButtonProps = {
+  children: React.ReactNode;
+  className?: string;
+  onClick?: () => void;
+};
+
+const Button: React.FC<ButtonProps> = ({
+  children,
+  className = "",
+  onClick,
+}) => {
+  return (
+    <button
+      className={`text-white px-4 py-2 m-1 rounded-lg ${className}`}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
