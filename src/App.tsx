0d40300bf35868a7b318598cc9969b561759f739
もっくん <mokkun@M3-MacBookAir.local>
2024-06-25 16:37:39 +0900

    タイマー仮完成

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,103 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Button from "./components/Button";
 
 function App() {
-  const [timer, setTimer] = useState(false);
-  const [isBreak, setIsBreak] = useState(false);
+  const workMin: number = 25;
+  const breakMin: number = 1;
+
+  const [min, setMin] = useState<number>(workMin);
+  const [sec, setSec] = useState<number>(0);
+
+  const [strSec, setStrSec] = useState<string>("00");
+
+  const [isActive, setIsActive] = useState<boolean>(false);
+  const [isBreak, setIsBreak] = useState<boolean>(false);
+
+  const [count, setCount] = useState<number>(0);
+
+  // ローカルストレージに保存したcountを呼び出し countに代入
 
   const timerStart = () => {
-    setTimer(true);
+    setIsActive(true);
+    setIsBreak(false);
   };
 
   const timerStop = () => {
-    setTimer(false);
+    setIsActive(false);
   };
+
+  const digitCheck = () => {
+    if (sec > 9) {
+      setStrSec(String(sec));
+    } else {
+      setStrSec("0" + String(sec));
+    }
+  };
+
+  useEffect(() => {
+    let interval: NodeJS.Timeout;
+
+    if (isActive) {
+      interval = setInterval(() => {
+        if (sec == 0) {
+          setMin(min - 1);
+          setSec(59);
+        } else {
+          setSec(sec - 1);
+          // console.log(sec - 1);
+        }
+        digitCheck();
+        clearInterval(interval);
+      }, 100);
+
+      // 時間が0になったときの処理
+      if (min == 0 && sec == 1) {
+        setIsActive(!isActive);
+        setIsBreak(!isBreak);
+        // タイマーをに設定休憩時間
+        setMin(breakMin);
+        setSec(0);
+      }
+    }
+
+    if (isBreak) {
+      interval = setInterval(() => {
+        if (sec == 0) {
+          setMin(min - 1);
+          setSec(59);
+        } else {
+          setSec(sec - 1);
+          // console.log(sec - 1);
+        }
+        digitCheck();
+        clearInterval(interval);
+      }, 100);
+
+      // 時間が0になったときの処理
+      if (min == 0 && sec == 1) {
+        setIsActive(!isActive);
+        setIsBreak(!isBreak);
+        // タイマーをワーク時間に設定
+        setMin(workMin);
+        setSec(0);
+        setCount(count + 1);
+      }
+    }
+  }, [isActive, isBreak, min, sec, count]);
+
   return (
     <div className="">
-      <div className="bg-gray-100 px-5 m-5 shadow-lg rounded-3xl">
-        <p className="text-center pt-5">
+      <div
+        className={`bg-gray-100 px-5 m-5 shadow-lg rounded-3xl ${
+          isBreak ? "bg-blue-100" : "bg-red-100"
+        }`}
+      >
+        <p className="text-xl text-center pt-5">
           {isBreak ? "Break Time" : "Work Time"}
         </p>
-        <p className="text-[120px] text-center leading-none pb-5">12:12</p>
+        <p className="text-[120px] text-center leading-none pb-5">
+          {min}:{strSec}
+        </p>
       </div>
       <div className="text-center">
         <Button className="bg-blue-400" onClick={timerStart}>
@@ -28,6 +107,25 @@ function App() {
           一時停止
         </Button>
         <Button className="bg-red-400">終了</Button>
+      </div>
+
+      <div>
+        <h1 className="text-2xl text-center mt-5">
+          あなたは、今まで{count}セット取り組みました!
+        </h1>
+        <div className="flex justify-center">
+          <p>回数はブラウザに保存されます。</p>
+          <p
+            className="text-red-500 underline cursor-pointer"
+            onClick={() => {
+              if (confirm("本当にリセットしますか?")) {
+                // TODO ローカルストレージ初期化する
+              }
+            }}
+          >
+            リセット
+          </p>
+        </div>
       </div>
     </div>
   );
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -13,7 +13,7 @@ const Button: React.FC<ButtonProps> = ({
 }) => {
   return (
     <button
-      className={`text-white px-4 py-2 m-1 rounded-lg ${className}`}
+      className={`text-white px-4 py-2 m-1 rounded-lg shadow-md hover:shadow-none ${className}`}
       onClick={onClick}
     >
       {children}
